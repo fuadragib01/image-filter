698a00174607e116974b02fec8ec1ffdcab860ad
Faad Ragib <ragibfuad@gmail.com>
2022-09-14 22:37:30 +0600

    Learning form submit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from "./App";
+// import App from "./App";
+import Form from "./components/Form";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot( document.getElementById( "root" ) );
 
-
-root.render( <App /> );
+root.render( <Form /> );
 
 reportWebVitals();
